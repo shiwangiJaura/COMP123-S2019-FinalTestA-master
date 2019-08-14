3ce6df2bd002f673864f2198d081d1becd26440d
301044130 <301044130@PR-E216-46>
2019-08-14 16:49:16 -0400

    Add project files.

---
+++ COMP123-S2019-FinalTestA/Views/HeroGenerator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+/*
+ * STUDENT NAME: 
+ * STUDENT ID:
+ * DESCRIPTION: This is the Here Generator Form
+ */
+
+namespace COMP123_S2019_FinalTestA.Views
+{
+    public partial class HeroGenerator : COMP123_S2019_FinalTestA.Views.MasterForm
+    {
+        public HeroGenerator()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// This is the event handler for the BackButton Click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            if (MainTabControl.SelectedIndex != 0)
+            {
+                MainTabControl.SelectedIndex--;
+            }
+        }
+
+        /// <summary>
+        /// This is the event handler for the NextButton Click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            if(MainTabControl.SelectedIndex < MainTabControl.TabPages.Count - 1)
+            {
+                MainTabControl.SelectedIndex++;
+            }
+        }
+    }
+}
